e71d559d11d668cfbbd6fd8847d9f18c3ad442bd
WildfireRanch <bret@westwood5.com>
2025-07-02 02:36:28 +0000

    jsj

--- frontend/src/app/gmail-ops/page.tsx
+++
@@ -1,7 +0,0 @@
-// File: app/gmail-ops/page.tsx
-
-"use client";
-import GmailOpsPanel from "@/components/GmailOps/GmailOpsPanel";
-export default function GmailOpsPage() {
-  return <GmailOpsPanel />;
-}
