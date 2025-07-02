12565ecb60dc8df742170ec920a79b17c5b1f79f
WildfireRanch <bret@westwood5.com>
2025-07-02 22:18:49 +0000

    css

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,8 @@
+// File: src/lib/utils.ts
+
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function testCN(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
