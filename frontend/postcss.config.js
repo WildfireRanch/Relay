231137c1e69c144d0a4c0f69704bf549f466d825
WildfireRanch <bret@westwood5.com>
2025-07-09 02:08:01 +0000

    frontend

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,10 +1,7 @@
 // postcss.config.mjs
-// File: frontend/postcss.config.mjs
-// This file is used to configure PostCSS, which processes CSS files.
-
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
+    autoprefixer: {}
+  }
 };
